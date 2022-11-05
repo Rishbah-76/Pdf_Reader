968d7be233f1624daa749b64fda445f4f0e08b12
Rishbah-76 <rbrishabh76@gmail.com>
2022-11-05 23:35:58 +0530

    st01

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "pdf_Reader"
+AUTHOR_USER_NAME = "Rishbah-76"
+SRC_REPO = "pdfReader"
+AUTHOR_EMAIL = "rbrishabh76@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package to rotate a page within a pdf file",
+    long_description=long_description,
+    long_description_content="text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src")
+)
---
+++ src/pdfReader/utils/__init__.py
@@ -0,0 +1,1 @@
+from pdfReader.utils.common import *
